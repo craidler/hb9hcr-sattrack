141fb13f14fc87f281b4fe188777d67bddf0d2ec
Christof Raidler HB9HCR <christof.raidler@gmail.com>
2026-02-09 21:14:58 +0100

    Update Motor.py

--- Motor.py
+++ Motor.py
@@ -20,9 +20,38 @@ class Motor:
         
         self.driver.Stop()
 
+    def ramp(self, steps, delay_min=.001, delay_max=.01):
+        if 0 == steps:
+            return
+
+        direction = 'forward'
+
+        if 0 > steps:
+            direction = 'backward'
+            steps = abs(steps)
+        
+        steps_ramp = min(20, steps / 2)
+
+        for i in range(steps):
+            if i < steps_ramp:
+                d = delay_max - (i / steps_ramp) * (delay_max - delay_min)
+            elif i >= (steps - steps_ramp):
+                c = i - (steps - steps_ramp)
+                d = delay_min + (c / steps_ramp) * (delay_max - delay_min)
+            else:
+                d = delay_min
+
+            self.driver.TurnStep(direction, 1, d)
+
+        self.driver.Stop()        
+
 
 if __name__ == '__main__':
     motor = Motor(13, 19, 12, (16, 17, 20))
     motor.linear(100)
     time.sleep(.2)
     motor.linear(-100)
+    time.sleep(1)
+    motor.ramp(100)
+    time.sleep(.2)
+    motor.ramp(-100)

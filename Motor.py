40f0629a51d56ae6cc197714938f68d2e7bfbced
Christof Raidler HB9HCR <christof.raidler@gmail.com>
2026-02-09 20:59:21 +0100

    Update Motor.py

--- Motor.py
+++ Motor.py
@@ -8,12 +8,21 @@ class Motor:
         self.driver = DRV8825(dir_pin, step_pin, enable_pin, mode_pins)
         self.driver.SetMicroStep(mode, step)
 
-    def linear(self, steps, direction='forward', delay=.001):
-        self.driver.TurnStep(direction, steps, delay)
+    def linear(self, steps, delay=.001):
+        if 0 == steps:
+            return 
+
+        if 0 < steps:
+            self.driver.TurnStep('forward', abs(steps), delay)
+
+        if 0 > steps:
+            self.driver.TurnStep('backward', abs(steps), delay)
+        
         self.driver.Stop()
 
 
 if __name__ == '__main__':
     motor = Motor(13, 19, 12, (16, 17, 20))
     motor.linear(100)
-    time.sleep(1)
+    time.sleep(.2)
+    motor.linear(-100)

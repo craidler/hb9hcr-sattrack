1b127c8402fa3f62cde9dd24a3935d2d617ca969
Christof Raidler HB9HCR <christof.raidler@gmail.com>
2026-02-10 10:11:04 +0100

    Update Motor.py

--- Motor.py
+++ Motor.py
@@ -1,85 +1,71 @@
-import math
-import time
-
-from DRV8825 import DRV8825
+import math, time
+import gpiozero as GPIO
 
 
 class Motor:
-    def __init__(self, dir_pin, step_pin, enable_pin, mode_pins, mode='softward', step='fullstep'):
-        self.driver = DRV8825(dir_pin, step_pin, enable_pin, mode_pins)
-        self.driver.SetMicroStep(mode, step)
+    PROFILE_TRAPEZE = 'trapeze'
+    PROFILE_PLATEAU = 'plateau'
+    PROFILE_LINEAR = 'linear'
+    PROFILE_SINUS = 'sinus'
 
-    def linear(self, steps, delay=.001):
+    def __init__(self, direction, step, enable, mode):
+        self.control = {
+            direction: GPIO.LED(direction),
+            enable: GPIO.LED(enable),
+            step: GPIO.LED(step),
+            mode[0]: GPIO.LED(mode[0]),
+            mode[1]: GPIO.LED(mode[1]),
+            mode[2]: GPIO.LED(mode[2]),
+        }
+
+    def turn(self, profile, steps, delay=.001):
+        steps = int(steps)
+
         if 0 == steps:
-            return 
+            return self
 
-        if 0 < steps:
-            self.driver.TurnStep('forward', abs(steps), delay)
+        self.write('direction', bool(steps))
+        self.write('enable', True)
 
-        if 0 > steps:
-            self.driver.TurnStep('backward', abs(steps), delay)
-        
-        self.driver.Stop()
+        steps = abs(steps)
 
-    def trapeziodal(self, steps, delay=.001, delay_max=.01):
-        if 0 == steps:
-            return
+        if self.PROFILE_LINEAR == profile:
+            for i in range(steps):
+                self.write('step', True)
+                time.sleep(delay)
+                self.write('step', False)
+                time.sleep(delay)
 
-        if 100 > abs(steps):
-            self.linear(steps, delay)
-            return
+        if self.PROFILE_SINUS == profile:
+            stretch = 10
+            steep = 10
+            for i in range(steps):
+                x = (i / steps) * 2 - 1
+                f = 1 / (1 + math.exp(-steep * (x + .5))) * (1 / (1 + math.exp(steep * (x - .5))))
+                d = delay * stretch - (f * (delay * stretch - delay))
+                self.write('step', True)
+                time.sleep(d)
+                self.write('step', False)
+                time.sleep(d)
 
-        direction = 'forward'
+        self.write('enable', False)
+        return self
 
-        if 0 > steps:
-            direction = 'backward'
-            steps = abs(steps)
-        
-        steps_ramp = min(50, steps / 2)
+    def write(self, pin, value):
+        if value:
+            self.control[pin].on()
+            return self
 
-        for i in range(steps):
-            if i < steps_ramp:
-                d = delay_max - (i / steps_ramp) * (delay_max - delay)
-            elif i >= (steps - steps_ramp):
-                c = i - (steps - steps_ramp)
-                d = delay + (c / steps_ramp) * (delay_max - delay)
-            else:
-                d = delay
-
-            self.driver.TurnStep(direction, 1, d)
-
-        self.driver.Stop()  
-
-    def sinusoidal(self, steps, delay=.001, delay_max=0.01):
-        if 0 == steps:
-            return
-        
-        direction = 'forward'
-        steepness = 10
-
-        if (0 > steps):
-            direction = 'backward'
-            steps = abs(steps)
-        
-        for i in range(steps):
-            x = (i / steps) * 2 - 1
-            f = 1 / (1 + math.exp(-steepness * (x + .5))) * (1 / (1 + math.exp(steepness * (x - .5))))
-            d = delay_max - (f * (delay_max - delay))
-            self.driver.TurnStep(direction, 1, d)
-        
-        self.driver.Stop()
+        self.control[pin].off()
+        return self
 
 
 if __name__ == '__main__':
-    motor = Motor(13, 19, 12, (16, 17, 20), 'softward', '1/32step')
-    motor.linear(400)
+    motor = Motor(13, 19, 12, (16, 17, 20))
+    motor.turn(motor.PROFILE_LINEAR, 400)
     time.sleep(.2)
-    motor.linear(-400)
+    motor.turn(motor.PROFILE_LINEAR, -400)
     time.sleep(1)
-    motor.trapeziodal(400)
+    motor.turn(motor.PROFILE_SINUS, 400)
     time.sleep(.2)
-    motor.trapeziodal(-400)
-    time.sleep(1)
-    motor.sinusoidal(400)
-    time.sleep(.2)
-    motor.sinusoidal(-400)
+    motor.turn(motor.PROFILE_SINUS, -400)

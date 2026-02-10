3c0984c93348c56f8147a6231fc527e508c94b6b
Christof Raidler HB9HCR <christof.raidler@gmail.com>
2026-02-10 10:21:37 +0100

    Update Motor.py

--- Motor.py
+++ Motor.py
@@ -3,12 +3,16 @@ import gpiozero as GPIO
 
 
 class Motor:
-    PROFILE_TRAPEZE = 'trapeze'
-    PROFILE_PLATEAU = 'plateau'
-    PROFILE_LINEAR = 'linear'
-    PROFILE_SINUS = 'sinus'
+    PROFILE_LINEAR = 'L'
+    PROFILE_PLATEAU = 'P'
+    PROFILE_SINUS = 'S'
+    PROFILE_TRAPEZE = 'T'
 
     def __init__(self, direction, step, enable, mode):
+        self.pin_direction = direction
+        self.pin_enable = enable
+        self.pin_step = step
+
         self.control = {
             direction: GPIO.LED(direction),
             enable: GPIO.LED(enable),
@@ -18,22 +22,50 @@ class Motor:
             mode[2]: GPIO.LED(mode[2]),
         }
 
+    '''
+    def trapeze(self, steps, delay=.001, delay_max=.01):
+        if 100 > abs(steps):
+            self.linear(steps, delay)
+            return
+
+        direction = 'forward'
+
+        if 0 > steps:
+            direction = 'backward'
+            steps = abs(steps)
+
+        steps_ramp = min(50, steps / 2)
+
+        for i in range(steps):
+            if i < steps_ramp:
+                d = delay_max - (i / steps_ramp) * (delay_max - delay)
+            elif i >= (steps - steps_ramp):
+                c = i - (steps - steps_ramp)
+                d = delay + (c / steps_ramp) * (delay_max - delay)
+            else:
+                d = delay
+
+            self.driver.TurnStep(direction, 1, d)
+
+        return self
+    '''
+
     def turn(self, profile, steps, delay=.001):
         steps = int(steps)
 
         if 0 == steps:
             return self
 
-        self.write('direction', bool(steps))
-        self.write('enable', True)
+        self.write(self.pin_direction, bool(steps))
+        self.write(self.pin_enable, True)
 
         steps = abs(steps)
 
         if self.PROFILE_LINEAR == profile:
             for i in range(steps):
-                self.write('step', True)
+                self.write(self.pin_step, True)
                 time.sleep(delay)
-                self.write('step', False)
+                self.write(self.pin_step, False)
                 time.sleep(delay)
 
         if self.PROFILE_SINUS == profile:
@@ -43,12 +75,12 @@ class Motor:
                 x = (i / steps) * 2 - 1
                 f = 1 / (1 + math.exp(-steep * (x + .5))) * (1 / (1 + math.exp(steep * (x - .5))))
                 d = delay * stretch - (f * (delay * stretch - delay))
-                self.write('step', True)
+                self.write(self.pin_step, True)
                 time.sleep(d)
-                self.write('step', False)
+                self.write(self.pin_step, False)
                 time.sleep(d)
 
-        self.write('enable', False)
+        self.write(self.pin_enable, False)
         return self
 
     def write(self, pin, value):
